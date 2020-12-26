a5aa199e94b39910cc4adb330431e1d9c4a3ba75
berkeka <berkekalkantr@gmail.com>
2020-12-26 16:59:48 +0300

    Create login window and crude logic

---
+++ Market/LogInWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Market
+{
+    /// <summary>
+    /// Interaction logic for LogInWindow.xaml
+    /// </summary>
+    public partial class LogInWindow : Window
+    {
+        public LogInWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButtonClicked(object sender, RoutedEventArgs e)
+        {
+            // Get inputs from UsernameText PasswordText text boxes
+
+            // Check if credientials are correct
+
+            // if correct close window
+            // this.DialogResult = true;
+
+            // else try again
+
+            this.DialogResult = false;
+
+
+
+        }
+    }
+}
