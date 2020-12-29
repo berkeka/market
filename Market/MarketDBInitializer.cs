b4809a740b67bf6ebe69e4781877d4bbcc7d10ff
berkeka <berkekalkantr@gmail.com>
2020-12-29 23:06:06 +0300

    Fix minor issue

--- Market/MarketDBInitializer.cs
+++ Market/MarketDBInitializer.cs
@@ -16,7 +16,7 @@ namespace Market
             {
                 User User1 = new User("Admin", "12345", "admin", "admin");
                 Product Product1 = new Product("1", "a", 1);
-                Customer Customer1 = new Customer("Berke", "Kalkan");
+                Customer Customer1 = new Customer("Berke", "Kalkan", 1234567890);
 
                 context.Users.Add(User1);
                 context.Products.Add(Product1);
