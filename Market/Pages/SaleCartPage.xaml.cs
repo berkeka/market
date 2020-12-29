ecefde603d24bfe59a34305049ea10fa54b08b84
berkeka <berkekalkantr@gmail.com>
2020-12-29 18:42:11 +0300

    Move ProductItem to entities

--- Market/Pages/SaleCartPage.xaml.cs
+++ Market/Pages/SaleCartPage.xaml.cs
@@ -176,19 +176,4 @@ namespace Market
 
     }
 
-    class ProductItem : Product
-    {
-        public int Amount { get; set; }
-
-        public ProductItem() { }
-        public ProductItem(Product p, int Amount)
-        {
-            this.Barcode = p.Barcode;
-            this.ID = p.ID;
-            this.Name = p.Name;
-            this.Price = p.Price;
-            this.Amount = Amount;
-        }
-
-    }
 }
