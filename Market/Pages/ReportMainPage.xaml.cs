221d793b8796bf1d4a6577a380cbc318535e1851
berkeka <berkekalkantr@gmail.com>
2021-01-02 00:03:00 +0300

    Create report main page

---
+++ Market/Pages/ReportMainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+
+namespace Market.Pages
+{
+    /// <summary>
+    /// Interaction logic for ReportMainPage.xaml
+    /// </summary>
+    public partial class ReportMainPage : Page
+    {
+        public ReportMainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButtonClicked(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+
+            MainWindow new_main = new MainWindow();
+
+            main.Title = new_main.Title;
+            main.Content = new_main.Content;
+            // Close the newly initialized window
+            new_main.Close();
+        }
+    }
+}
