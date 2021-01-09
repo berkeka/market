b7721415e5e6514bb066286fd1d25df0f4e7e46c
berkeka <berkekalkantr@gmail.com>
2021-01-09 18:51:12 +0300

    Rebase onto develop and fix conflicts

--- Market/Pages/SalePage.xaml.cs
+++ Market/Pages/SalePage.xaml.cs
@@ -36,7 +36,7 @@ namespace Market
                 {
                     var temp = queryPrd.Find(i);
                     var tempp = queryStock.Find(temp.Barcode);
-                    int amountt = tempp.Amount;
+                    int amountt = (int)tempp.Amount;
                     if(amountt > 100) { amountt = 100; }
                     items.Add(new StockItem() { Title = temp.Name, Completion = amountt });
                 }
