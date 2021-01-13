de003dcd9d2c8293046c058c182267740d611bc5
doguk <dogukan.hacifazlioglu@bil.omu.edu.tr>
2021-01-13 04:06:52 +0300

    Add SaleRecordList

--- Market/Pages/SaleRecordPage.xaml.cs
+++ Market/Pages/SaleRecordPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Market.Entities;
 
 namespace Market.Pages
 {
@@ -23,10 +24,33 @@ namespace Market.Pages
         public SaleRecordPage()
         {
             InitializeComponent();
+
+            var context = new MarketDBContext();
+
+            var querySale = context.Sales;
+            var queryCstmr = context.Customers;
+
+            if (querySale.Any())
+            {
+                List<SaleItem> si = new List<SaleItem>();
+                foreach(Sale sale in querySale.ToList())
+                {
+                    var cstmr = queryCstmr.Find(sale.CustomerIDNumber);
+                    string cFullName = cstmr.Name + " " + cstmr.LastName;
+                    si.Add(new SaleItem() { FullName = cFullName, ID = sale.ID, Date = sale.Date });
+                }
+                SaleRecordList.ItemsSource = si.OrderBy(i => i.Date);
+            }
         }
         private void SilButtonClicked(object sender, RoutedEventArgs e)
         {
 
+        }
+        public class SaleItem
+        {
+            public string FullName { get; set; }
+            public int ID { get; set; }
+            public DateTime Date { get; set; }
         }
         //Go back to sale page
         private void GoBackButtonClicked(object sender, RoutedEventArgs e)
